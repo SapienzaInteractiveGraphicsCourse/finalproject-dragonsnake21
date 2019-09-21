03db4a325996fd7ad04775cadb0115b70666b8cb
Flavio Lorenzi <43888007+FlavioLorenzi@users.noreply.github.com>
2019-09-21 11:35:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -166,6 +166,7 @@ class Game {
                     sound.play();
                 });
             }
+		//NOT USED
             if (song == 3) { //GAME OVER
                 audioLoader.load('sounds/gameover.mp3', function (buffer) {
                     sound.setBuffer(buffer);
