6edf075771731259f957198d315df83c6df24ae7
Michele <31963799+micheleciciolla@users.noreply.github.com>
2019-09-22 21:00:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -474,7 +474,7 @@ function main() {
     duck = new Duck(new THREE.Vector3(20, 2, 20));
     duck.build();
 
-    sheep = new Sheep(new THREE.Vector3(0, 2, -20));
+    sheep = new Sheep(new THREE.Vector3(6, 2, -20));
     sheep.build();
 
     cloud = new Cloud(new THREE.Vector3(0, 12, 0));
